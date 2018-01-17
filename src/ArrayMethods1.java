7777e9ea828806ec151836ea87cd679458ab592f
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2018-01-17 09:16:30 -0500

    swap

---
+++ src/ArrayMethods1.java
@@ -0,0 +1,15 @@
+
+public class ArrayMethods1 
+{
+	public static void swap (int []arr, int index1, int index2)
+	{
+		int temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp; 
+	}
+	
+	public static void insertionSort(int [] list1)
+	{
+		
+	}
+}
