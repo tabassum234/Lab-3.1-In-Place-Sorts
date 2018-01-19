0f1c352a3729b6e4e4204e1399cb6575a03bf65d
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-01-19 09:17:56 -0500

    3 methods

--- src/ArrayMethods1.java
+++ src/ArrayMethods1.java
@@ -1,38 +1,52 @@
 
 public class ArrayMethods1 
 {
-	public static void swap (int []arr, int index1, int index2)
+	public static void swap (int []list1, int index1, int index2)
 	{
-		int temp = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] = temp; 
+		int temp = list1[index1];
+		list1[index1] = list1[index2];
+		list1[index2] = temp; 
 	}
 	
 	public static void insertionSort(int [] list1)
 	{	
-		for (int i=1; 1< list1.length; i++)
-		{
-			int temp = list1[1];
-			int j;
-			for(j= i-1; j >=0 && temp < list1[j]; j--)
-				list1[j +1]= list1[1];
-			list1[j+1] = temp; 
-		}
-	}
-	
+        int n = list1.length;
+        
+        for (int i=1; i<n; ++i)
+        {
+            int key = list1[i];
+            int j = i-1;
+ 
+            while (j>=0 && list1[j] > key)
+            {
+                list1[j+1] = list1[j];
+                j = j-1;
+            }
+            list1[j+1] = key;
+        }
+    }
 	public static void selectionSort (double [] list1)
 	{
-		int temp = list1[1];
-		int size = list1.length;
-		for (i = 0; i< list1.length, i++)
-		{
-			min = i;
-		}
-		for (j = i +1 ; j < size; j++)
-		{
-			if List[j] < List[i]
-		}
+		for (i = 0; i < n-1; i++)
+	    {
+	        min= i;
+	        for (j = i+1; j < n; j++)
+	          if (list1[j] < list1[min_idx])
+	            min = j;
+	        swap(list1[min], & list1[i]);
+	    }
 	}
 	
-	public static void bubbleSort (String[] list1)
+	public static void bubbleSort (String [] list1)
+	{
+		int n = list1.length;
+		 for (int i = 0; i < n-1; i++)
+		   for (int j = 0; j < n-i-1; j++)
+		      if (list1[j] > list1[j+1])
+		        {
+		          int temp = list1[j];
+		          list1[j] = list1[j+1];
+		          list1[j+1] = temp;
+		        }
+	}
 }
